79c52170d9470fe4aec58ab3b57972453bd3bb3d
Ivan Neschadin <ipneschadin@avito.ru>
2023-09-26 22:33:07 +0300

    Init

---
+++ pages/resume.vue
@@ -0,0 +1,62 @@
+<script setup>
+useHead({
+  title: 'Resume',
+})
+</script>
+
+<template>
+  <article class="resume active" data-page="resume">
+    <header>
+      <h2 class="h2 article-title">
+        {{ $t('pageTitles.resume') }}
+      </h2>
+    </header>
+
+    <section class="timeline">
+      <ol class="timeline-list">
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">
+            Company
+          </h4>
+          <span>From — Until</span>
+          <p class="timeline-item-desc">
+            Location
+          </p>
+          <p class="timeline-text">
+          </p>
+        </li>
+        
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">
+            Simex inc.
+          </h4>
+          <span>10.2016 - 11.2017</span>
+          <p class="timeline-item-desc">
+            Moscow
+          </p>
+          <p class="timeline-text">
+            Junior Fullstack Developer
+          </p>
+        </li>
+      </ol>
+    </section>
+
+    <section class="skill">
+      <ul class="skills-list content-card">
+        <li class="skills-item">
+          <div class="title-wrapper">
+            <h5 class="h5">
+              Your Skill
+            </h5>
+            <data value="90">90%</data>
+          </div>
+
+          <div class="skill-progress-bg">
+            <div class="skill-progress-fill" style="width: 90%;" />
+          </div>
+        </li>
+
+      </ul>
+    </section>
+  </article>
+</template>
